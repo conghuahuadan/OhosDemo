7dde57379d5213032eaa8e1f19c31ca84dfb0cc1
陈伟强 <15875006020@qq.com>
2026-03-18 16:03:16 +0800

    upload.sh

--- upload.sh
+++ upload.sh
@@ -1,2 +1,4 @@
+ohpm install --all --registry https://ohpm.openharmony.cn/ohpm/ --strict_ssl true
+hvigorw --sync -p product=default --analyze=normal --parallel --incremental --daemon
 hvigorw assembleHap -p product=internal -p buildMode=debug
 groovy upload_hap.groovy
